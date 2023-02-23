14c734ed0070019a4df94c86598a3f796523490a
Kshitij Patil <114834634+Kshitijp989@users.noreply.github.com>
2023-02-23 16:29:12 +0530

    Initial commit

---
+++ EmployeeInfo/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="* Name cannot be blank")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "* City cannot be blank")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "* State cannot be blank")]
+        public string State { get; set; }
+        [Required]
+        public decimal? Salary { get; set; }
+
+    }
+}
